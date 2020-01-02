16b0aae59b2b53e2a080d31d3c6be9e94f31cba1
Steven Hoyt <steven.hoyt@att.net>
2020-01-02 13:43:27 -0600

    update

--- application/controllers/index.js
+++ application/controllers/index.js
@@ -3,17 +3,17 @@
 const electron    = require('electron')
 const ipcRenderer = electron.ipcRenderer
 document.addEventListener(
-                        'DOMContentLoaded'  ,
-                        function () {ipcRenderer.send('main-window-loaded')}
-                        )
+                            'DOMContentLoaded'  ,
+                            function () {ipcRenderer.send('main-window-loaded') }
+                         )
 ipcRenderer.on(
                 'results-sent'  ,
                 function(event, results) {                     
-                var people  = document.getElementById('people')
-                var html    = '';
-                for (var i = 0; i < results.length; i++) {
-                    html += '<span class="person">' + results[i].firstName + ' ' + results[i].lastName + '</span>'
-                }
-                people.innerHTML = html
+                    var people  = document.getElementById('people')
+                    var html    = '';
+                    for (var i = 0; i < results.length; i++) {
+                        html += '<span class="person">' + results[i].firstName + ' ' + results[i].lastName + '</span>'
+                    }
+                    people.innerHTML = html
                 }
             )    
